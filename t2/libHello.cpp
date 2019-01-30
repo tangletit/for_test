7c3ecce638c7d76f03731a0ed9436ef8b1e71b78
tangletit <646277312@qq.com>
2019-01-30 21:03:01 +0800

    youmeiyougaocuo

---
+++ t2/libHello.cpp
@@ -0,0 +1,8 @@
+#include "iostream"
+using namespace std;
+
+void printHello()
+{
+	cout <<"hello slam ,please!"<< endl;
+	
+}
---
+++ t2/useHello.cpp
@@ -0,0 +1,9 @@
+#include "libHello.h"
+#include "iostream"
+using namespace std;
+int main ()
+{
+	printHello();
+	cin.get();
+	return 0;
+}
